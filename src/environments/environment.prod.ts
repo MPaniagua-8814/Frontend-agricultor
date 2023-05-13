48ddac8ac047029a6861a58880b73f2616fdf67b
MPaniagua-8814 <mpaniagual@miumg.edu.gt>
2023-05-13 09:49:25 -0600

    Se agregar ruta de servicios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  rutaMicros: 'https://beneficio-cafetito-ws.herokuapp.com'
+  rutaMicros: 'https://beneficio-cafetito-ws.herokuapp.com',
+  serviciosPrueba: 'http://10.123.0.57:8080'
 };
